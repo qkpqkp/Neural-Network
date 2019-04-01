c669d82361ee42746655e65747818acbde0c0d1c
qkpqkp <qinkunpeng2015@163.com>
2019-04-01 10:59:41 -0500

    Update RNN_Model.py

--- codes/RNN_Model.py
+++ codes/RNN_Model.py
@@ -19,9 +19,10 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-df = parse('D:\\UT Courses\\EE 364D\\Office\\run/eplusout.sql')
+df = parse('D:\\UT Courses\\EE 364D\\Office1\\run/eplusout.sql')
 df.head()
 print(df.shape)
+
 
 names=df.columns.values.tolist()
 y=pd.DataFrame()
@@ -37,20 +38,18 @@ x=x.transpose().values
 x=x[:,np.newaxis,:]
 y=y.transpose().values
 batch_size=40
-epochs=100
+epochs=1000
 print(x.shape)
 print(y.shape)
 
 def create_rnn_model():
     model = keras.models.Sequential()
-    model.add(keras.layers.SimpleRNN(y.shape[1],input_shape=(1,x.shape[2],),activation="relu"))
+    model.add(keras.layers.SimpleRNN(y.shape[1],input_shape=(1,x.shape[2],),activation="relu",dropout=0.5,recurrent_dropout=0.5))
+    model.add(keras.layers.Dense(y.shape[1]))
     model.set_weights([np.random.rand(*w.shape)*0.2 - 0.1 for w in model.get_weights()])
     return model
 
 
 model_RNN = create_rnn_model()
-model_RNN.compile(optimizer='adam', loss='mean_squared_error')
+model_RNN.compile(optimizer='sgd', loss='mean_squared_error')
 model_RNN.fit(x,y,batch_size=batch_size,epochs=epochs)
-
-
-
