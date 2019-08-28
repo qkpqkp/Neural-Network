33534b4588672fa6ebd2865a6cbe3fa91be81bee
qkpqkp <qinkunpeng2015@163.com>
2019-08-28 07:41:07 -0500

    RNN_MODEL

--- codes/RNN_Model.py
+++ codes/RNN_Model.py
@@ -15,67 +15,70 @@ import keras
 import keras.backend as K
 from sklearn.preprocessing import normalize
 from eplusparser.eplusparser import parse
-import pandas as pd
-import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-
-
-
-df = parse('D:\\UT Courses\\EE 364D\\Office\\run/eplusout.sql')
-df1 = parse('D:\\UT Courses\\EE 364D\\Office1\\run/eplusout.sql')
-df.head()
-df1.head()
-print(df.shape)
-scaler = MinMaxScaler()
-normalized_df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-normalized_df = normalized_df.dropna(axis='columns')
-#normalized_df = normalized_df.loc[:, (normalized_df != 0).any(axis=0)]
-df=normalized_df
-names=df.columns.values.tolist()
-y=pd.DataFrame()
-x=pd.DataFrame()
-for i in range(3,len(names)):
-    if "Setpoint" in names[i][1] or "Schedule" in names[i][1] or "Occupant" in names[i][1] or "Outdoor" in names[i][1]:
-        x=x.append(df.iloc[:,i],ignore_index=True)
-    else:
-        y=y.append(df.iloc[:,i],ignore_index=True)
-
-names1=df1.columns.values.tolist()
-x_test=pd.DataFrame()
-y_test=pd.DataFrame()
-
-for i in range(3,len(names1)):
-    if "Setpoint" in names1[i][1] or "Schedule" in names1[i][1] or "Occupant" in names1[i][1] or "Outdoor" in names1[i][1]:
-        x_test=x_test.append(df1.iloc[:,i],ignore_index=True)
-    else:
-        y_test=y_test.append(df1.iloc[:,i],ignore_index=True)
-
-x=x.transpose().values
-x=x[:,np.newaxis,:]
-y=y.transpose().values
-batch_size=40
-epochs=200
-print(x.shape)
-print(y.shape)
-
-x_test=x_test.transpose().values
-x_test=x_test[:,np.newaxis,:]
-y_test=y_test.transpose().values
-
-def create_rnn_model():
-    model = keras.models.Sequential()
-    model.add(keras.layers.SimpleRNN(y.shape[1],input_shape=(1,x.shape[2]),activation="relu"))
-    #model.add(keras.layers.SimpleRNN(y.shape[1]))
+from keras.regularizers import l2
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+def create_dataset(dataset,names,scaler,look_back=1):
+    dataX, dataY = [], []
     
-    model.set_weights([np.random.rand(*w.shape)*0.2 - 0.1 for w in model.get_weights()])
+    for i in range(3,len(names)):
+        if "Setpoint" in names[i][1] or "Schedule" in names[i][1] or "Occupant" in names[i][1] or "Outdoor" in names[i][1]:
+            dataX.append(dataset[1:,i])
+        
+        else:
+            dataX.append(dataset[:len(dataset)-1,i])
+    dataY.append(dataset[1:,1])
+    dataY.append(dataset[1:,2])
+    dataX=np.array(list(map(list,zip(*dataX))))
+    dataX=scaler.fit_transform(dataX)
+    dataX=dataX[:,np.newaxis,:]
+    dataY=np.array(list(map(list,zip(*dataY))))
+    dataY=scaler.fit_transform(dataY)
+    return dataX, dataY
+
+def create_model(trainX,trainY,look_back=1):
+    model = keras.models.Sequential()
+    model.add(keras.layers.LSTM(36,input_shape=(look_back,trainX.shape[2]),activation="sigmoid",return_sequences=True))
+    model.add(keras.layers.Dropout(0.1))
+    model.add(keras.layers.LSTM(36,input_shape=(look_back,trainX.shape[2]),activation="sigmoid",return_sequences=True))
+    model.add(keras.layers.LSTM(18,input_shape=(look_back,trainX.shape[2]),activation="sigmoid",return_sequences=True))
+    model.add(keras.layers.LSTM(trainY.shape[1], input_shape=(look_back, trainX.shape[2]),activation="sigmoid"))
+    #model.add(keras.layers.Dropout(0.1))
+    model.add(keras.layers.Dense(trainY.shape[1]))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
     return model
 
+df = parse('D:\\UT Courses\\EE 364D\\Office\\run/eplusout.sql')
+df.head()
+dataset=df.values
+names=df.columns.values.tolist()
 
-model_RNN = create_rnn_model()
-rmsprop=keras.optimizers.rmsprop(lr=0.001)
-model_RNN.compile(optimizer=rmsprop, loss='mean_squared_error', metrics=['accuracy'])
-model_RNN.fit(x,y,batch_size=batch_size,epochs=epochs)
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
 
-score, acc = model_RNN.evaluate(x_test, y_test, batch_size=batch_size)
+look_back = 1
+trainX, trainY = create_dataset(dataset, names,scaler, look_back)
 
+df1 = parse('D:\\UT Courses\\EE 364D\\Office1\\run/eplusout.sql')
+df1.head()
+testset = df1.values
+
+names1=df1.columns.values.tolist()
+testX, testY = create_dataset(testset, names1,scaler, look_back)
+
+model=create_model(trainX,trainY)
+model.fit(trainX, trainY, epochs=100, batch_size=10)
+
+
+
+
+score, acc = model.evaluate(testX, testY, batch_size=10)
 print('Test accuracy:', acc)
+testPredict = model.predict(testX)
+testPredict = scaler.inverse_transform(testPredict)
+testY=scaler.inverse_transform(testY)
+print(testPredict)
+print(testY)
+rmse = sqrt(mean_squared_error(testPredict,testY))
+print(rmse)
